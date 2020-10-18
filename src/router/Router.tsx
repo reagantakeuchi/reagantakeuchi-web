667ff8a5d1aee0076f60b597ca0a763f534ab1bb
Reagan Takeuchi <reagan.tak@gmail.com>
2020-10-17 23:13:57 -0500

    Add routing for contact and construction

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -16,6 +16,15 @@ export const NavSwitch = () => (
             <Router.Route path="/projects">
                 <UnderConstruction/>
             </Router.Route>
+            <Router.Route path="/resume">
+                <UnderConstruction/>
+            </Router.Route>
+            <Router.Route path="/portfolio">
+                <UnderConstruction/>
+            </Router.Route>
+            <Router.Route path="/contact">
+                <ContactMe/>
+            </Router.Route>
         </Router.Switch>
     </Suspense>
 )
@@ -23,3 +32,4 @@ export const NavSwitch = () => (
 const Landing = React.lazy(() => import('../sections/landing'));
 const Transition = React.lazy(() => import('../sections/transition'));
 const UnderConstruction = React.lazy(() => import('./pages/under-construction'));
+const ContactMe = React.lazy(() => import('./pages/contact-me'))
