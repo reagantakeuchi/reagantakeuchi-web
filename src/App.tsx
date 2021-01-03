67fd4486128ba8aac5de9c03f1b3ef4cd9994d23
Reagan Takeuchi <reagan.tak@gmail.com>
2021-01-03 16:41:37 -0600

    fix react hook deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Context} from 'react';
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { Box, Grommet } from 'grommet';
@@ -6,11 +6,29 @@ import { NavSwitch } from './router/Router';
 import { theme } from './style/theme';
 import { Nav } from './components/nav';
 import styled from 'styled-components';
-import { DeviceContext, Devices, getDevice } from './device-context';
+import { DeviceContext, Devices } from './device-context';
+import { useMediaQuery } from 'react-responsive';
+
+
+function deviceSwitch (
+  devices:{desktop:boolean, bigScreen:boolean, mobile:boolean}
+): Devices{
+  const {desktop, bigScreen, mobile} = devices;
+
+  if(desktop) return 'desktop'
+  if(bigScreen) return 'bigScreen'
+  return 'mobile'
+}
 
 function App() {
-  const device = getDevice();
+  const media = {
+    desktop: useMediaQuery({query: '(min-device-width: 1224px)'}),
+    bigScreen: useMediaQuery({ query: '(min-device-width: 1824px)' }),
+    mobile: useMediaQuery({ query: '(max-width: 1224px)' }) 
+  }
 
+  const device = deviceSwitch(media);
+  
   return (
     <Grommet className="App" theme={theme} background={theme.global.colors['light-2']}>
         <Router>
